814cb6483ae6b0fd656ec8aae328083ee105db3f
ankitr305 <ankitrajurkar03@gmail.com>
2021-09-12 21:42:19 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,47 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Template Driven Form</h1><br><br>
+  <div>
+    <h2>User Data</h2>
+    <form #userForm = "ngForm" (ngSubmit)="onSubmit(userForm.value)">
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control" name="name" [(ngModel)]="myname">
+        <p>{{myname}}</p>
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text" class="form-control" name="email" [ngModel]="myemail">
+      </div>
+      <div ngModelGroup="Address">
+        <div class="form-group">
+          <label>Street</label>
+          <input type="text" class="form-control" name="street" ngModel>
+        </div>
+        <div class="form-group">
+          <label>City</label>
+          <input type="text" class="form-control" name="city" ngModel>
+        </div>
+        <div class="form-group">
+          <label>Postal Code</label>
+          <input type="text" class="form-control" name="code" ngModel>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+  </div>
+
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TemplateDrivenForm';
+  myname = "Ankit"
+  myemail = "ankit@email.com"
+  onSubmit(value:any){
+    console.log(value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMenuModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
